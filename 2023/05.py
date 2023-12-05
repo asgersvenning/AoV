cf509af3a86e78f4c7ce7dd8db190a007df3778a
asgersvenning <asgersvenning@gmail.com>
2023-12-05 23:12:28 +0000

    Cleaned day 5

--- 2023/05.py
+++ 2023/05.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-
 class Range:
     def __init__(self, string : str) -> list[int]:
         string = string.strip().split(" ")
@@ -16,22 +13,11 @@ class Range:
                 return n + self.offset, (self.source + self.length - 1) - n
             else:
                 return n + self.offset
-        else:
-            raise ValueError(f"{n} is not in range {self}")
-
-    def __str__(self):
-        return f"{self.source} -> {self.destination} ({self.length})"
-    
-    def invert(self):
-        return Range(f"{self.source} {self.destination} {self.length}")
-    
 
 class Map:
     def __init__(self, string : str):
-        # print(string)
         lines = string.strip().splitlines()
         title = lines.pop(0)
-        # print(lines)
         self.name = title.removesuffix(" map:")
         self.ranges = [Range(line) for line in lines]
         self.get_remainder = False
@@ -43,97 +29,45 @@ class Map:
         else:
             if self.get_remainder:
                 distances = [r.source - key for r in self.ranges if r.source > key]
-                if len(distances) > 0:
-                    remainder = min(distances)
-                else:
-                    remainder = "inf"
+                remainder = min(distances) if len(distances) > 0 else "inf"
                 return key, remainder
             else:
                 return key
-        
-    def min_key(self):
-        """
-        This function finds the key that is mapped to the smallest value.
-
-        The minimum key is 0 and the minimum value is also 0.
-        """
-        for r in self.ranges:
-            if r.destination == 0:
-                return r.source
-        else:
-            return 0
-        
-    def __str__(self):
-        return self.name + ": " + ", ".join([str(r) for r in self.ranges])
-    
-    def invert(self):
-        inverse_ranges = [r.invert() for r in self.ranges]
-        new_name = self.name.split("-")[::-1]
-        new_name = "-".join(new_name)
-        inverse_map = Map(new_name + " map:")
-        inverse_map.ranges = inverse_ranges
-        return inverse_map
     
 def length_of_step(remainders : list) -> int:
-    """
-    This function takes a list of remainders and returns the amount of steps left before the next jump.
-    """
     remainders = [r for r in remainders if r != "inf"]
     return min(remainders)
     
 path = "inputs/05.input"
 
 with open(path, "r") as f:
-    # Part 1
+    # Part 1 - Calculate all locations
     data = f.read().split("\n\n")
     seeds = data[0].strip().removeprefix("seeds: ").split(" ")
     seeds = [int(s) for s in seeds]
-
     maps = [Map(m) for m in data[1:]]
 
     locations = []
     for seed in seeds:
+        intermediary = seed
         for m in maps:
-            seed = m[seed]
-        locations.append(seed)
+            intermediary = m[intermediary]
+        locations.append(intermediary)
     print(min(locations))
 
-    # Part 2
-    #inverse_maps = [m.invert() for m in reversed(maps)]
-    for m in maps:
-        m.get_remainder = True
+    # Part 2 - Skip linear ranges
+    for map in maps:
+        map.get_remainder = True
     seed_ranges = [[i, j] for i, j in zip(seeds[::2], seeds[1::2])]
-    initial_seeds = []
     locations = []
-    old_location = None
     for start, length in seed_ranges:
         seed = start
-        did_jump = False
         while seed < start + length:
-            initial_seeds += [seed]
-            remainders = []
-            intermediary = seed
-            for m in maps:
-                intermediary, remaining = m[intermediary]
+            intermediary, remainders = seed, []
+            for map in maps:
+                intermediary, remaining = map[intermediary]
                 remainders.append(remaining)
-            if not did_jump:
-                # Jump to the next seed (last in the current linear range)
-                seed += length_of_step(remainders)
-                did_jump = True
-            else:
-                # Jump to the next seed (first in the next linear range)
-                seed += 1
-                did_jump = False
-            
-            # Score for the current seed
-            location = intermediary
-            locations.append(location)
+            seed += length_of_step(remainders) + 1
+    
+            locations.append(intermediary)
     print(min(locations))
-
-
-
-
-
-
-
-
