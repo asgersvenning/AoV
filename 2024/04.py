9664e94bf5cf501d40d1ecfba0aa56581dcb75eb
asgersvenning <asgersvenning@gmail.com>
2024-12-08 17:32:10 +0100

    Day 4 done! (very readable lol...)

--- 2024/04.py
+++ 2024/04.py
@@ -8,55 +8,45 @@
 # Restate:
 #   Given a matrix of characters, find all sequences "XMAX" either forward, backward and diagonally.
 
-import torch
-import re
-from itertools import cycle
-from matplotlib import pyplot as plt
+import torch, re
 
-CMAP = {
-    "X" : 10,
-    "M" : 100,
-    "A" : 1000,
-    "S" : 10000,
-    "." : -1
-}
-
-PATTERN = 10 ** torch.tensor([1, 2, 3, 4])
-MATCH = (PATTERN ** 2).sum().item()
+CMAP = {"X" : 2 ** (1/2), "M" : 3 ** (1/2), "A" : 5 ** (1/2), "S" : 7 ** (1/2), "." : 0}
 
 def parse_input(path):
     with open(path, "r") as f:
-        input =  list(map(lambda x : re.sub("[^XMAS]", ".", x.strip()), f.readlines()))
-    return torch.tensor([[CMAP[c] for c in line] for line in input]).float()
+        return torch.tensor([[CMAP[c] for c in line] for line in map(lambda x : re.sub("[^XMAS]", ".", x.strip()), f.readlines())]).float()
 
-kern_flat = torch.zeros([1, 4]) + PATTERN
-kern_diag = torch.eye(4) * PATTERN
-
-def matches(map, kern):
-    osmap = map.shape
-    mdimmap = max(0, 4 - len(osmap))
-    if mdimmap:
-        map = map.view(*[1] * mdimmap, *osmap)
-    oskern = kern.shape
-    mdimkern = max(0, 4 - len(oskern))
-    if mdimkern:
-        kern = kern.view(*[1] * mdimkern, *oskern)
-    return (torch.nn.functional.conv2d(map, kern) == MATCH).sum(dim = 1).view(*[md - (kd - 1) for md, kd in zip(osmap, cycle(kern.shape[2:]))])
+def matches(map : torch.Tensor, kern : torch.Tensor):
+    map = map.view(*[1] * max(0, 4 - len(map.shape)), *map.shape)if max(0, 4 - len(map.shape)) else map
+    kern = kern.view(*[1] * max(0, 4 - len(kern.shape)), *kern.shape) if max(0, 4 - len(kern.shape)) else kern       
+    return torch.isclose(torch.nn.functional.conv2d(map, kern, padding="same"), (kern ** 2).sum(2, keepdim=True).sum(3, keepdim=True).squeeze(1).unsqueeze(0))
 
 def part1(t_map : torch.Tensor) -> int:
-    kern_fb = torch.stack([kern_flat, kern_flat.flip(1)]).unsqueeze(1)
-    kern_tb = kern_fb.transpose(3, 2)
-    kern_cr = torch.stack([kern_diag, kern_diag.flip(1), kern_diag.flip(0), kern_diag.flip(1).flip(0)]).unsqueeze(1)
-    # fig, axs = plt.subplots(2, 2)
-    # for ax, m in zip(axs.flatten(), kern_cr.view(4, 4, 4)):
-    #     ax.matshow(m.log())
-    # plt.show()
-    return sum(map(torch.sum, map(lambda k : matches(t_map, k), [kern_fb, kern_tb, kern_cr]))).item()
-    # maps = [t_map, t_map.flip(1), t_map.T, t_map.T.flip(1)]
-    # print()
-    # for m in maps:
-    #     print("\n".join(["".join([str(e.int().item()) for e in r]) for r in m]) + "\n")
-    # return [[matches(m, k).sum().item() for m in maps] for k in [kern_flat, kern_diag]]
+    PATTERN = torch.tensor([CMAP[c] for c in "XMAS"])
+    kern_flat = torch.zeros([5, 5]).index_put_((torch.zeros(4).long(), torch.arange(4)), PATTERN)
+    kern_diag = torch.zeros([5, 5]).index_put_((torch.arange(4), torch.arange(4)), PATTERN)
+    return matches(torch.nn.functional.pad(t_map, [5] * 4), torch.stack([kern_flat, kern_flat.flip(1), kern_flat.T, kern_flat.flip(1).T, kern_diag, kern_diag.flip(1), kern_diag.flip(0), kern_diag.flip(0).flip(1)]).unsqueeze(1)).squeeze(0).sum(1).sum(1).sum().item()
 
 print("Test (part 1):", part1(parse_input("2024/inputs/04.test")))
 print("Part 1:", part1(parse_input("2024/inputs/04.input")))
+
+# Problem:
+#   you're supposed to find two MAS in the shape of an X. 
+#   One way to achieve that is like this:
+#       M.S
+#       .A.
+#       M.S
+#   Irrelevant characters have again been replaced with . in the above diagram. 
+#   Within the X, each MAS can be written forwards or backwards.
+#   How many times does an X-MAS appear?
+# Restate:
+#   Same as before, but different pattern.
+
+def part2(t_map : torch.Tensor) -> int:
+    HALF_PATTERN = torch.tensor([CMAP[c] for c in "MAS"])
+    kerns = torch.zeros((4, 1, 3, 3))
+    [kerns[i + j * 2, 0].index_put_((torch.arange(3), torch.arange(3)), HALF_PATTERN if j == 0 else HALF_PATTERN.flip(0)) + kerns[i + j * 2, 0].index_put_((-torch.arange(1, 4), torch.arange(3)), HALF_PATTERN if i == 0 else HALF_PATTERN.flip(0)) for j in range(2) for i in range(2)]
+    return matches(torch.nn.functional.pad(t_map, [3] * 4), kerns).squeeze(0).sum(1).sum(1).sum().item()
+
+print("Test (part 2):", part2(parse_input("2024/inputs/04.test")))
+print("Part 2:", part2(parse_input("2024/inputs/04.input")))
