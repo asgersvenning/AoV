ca83a5fff5df7777fbaa571c02851f5cfc1f7c76
asgersvenning <asgersvenning@gmail.com>
2024-12-09 20:26:22 +0100

    Day 9: Removed unnecessary numpy and tqdm dependency and some refactoring

--- 2024/09.py
+++ 2024/09.py
@@ -1,7 +1,9 @@
-import os
 import bisect
+import os
+import time
+
 from sortedcontainers import SortedDict
-import numpy as np
+
 
 def parse_input(path : str):
     with open(path, "r") as file:
@@ -13,30 +15,18 @@ def get_input(type : str="test"):
     return parse_input(os.path.join(dir, "inputs", day + "." + type))
     
 def expand_view(data : list[int], ids : list[bool]) -> list[str]:
-    out = []
-    for size, id in zip(data, ids):
-        out.extend([str(id) if id != -1 else "."] * size)
-    return out
+    return [c for size, id in zip(data, ids) for c in [str(id) if id != -1 else "."] * size]
 
 def spans(indices, max):
-    breaks = np.where(np.diff(np.array(indices, dtype=np.int32), 1) > 1)[0].tolist()
+    breaks = [p for p, (i, j) in enumerate(zip(indices, indices[1:])) if (j - i) > 1]
     return SortedDict({indices[start + 1] : indices[start + 1] + end - start for start, end in zip(([-1] + breaks), (breaks + [max]))})
 
 class File:
     def __init__(self, id, disk, start=None, size=None, indices=None, maintain_disk : bool=True, with_spans : bool=False):
-        self.disk = disk
-        self._maintain_disk = maintain_disk
-        self.file = id != -1
-        self.id = id if self.file else None
-        if indices is None:
-            self.indices = list(range(start, start+size))
-        else:
-            if not (start is None and size is None):
-                raise ValueError("Cannot supply both indices and start/stop")
-            self.indices = sorted(indices)
-        self._with_spans = with_spans
-        if self._with_spans:
-            self.spans = spans(self.indices, len(self))
+        self._maintain_disk, self._with_spans = maintain_disk, with_spans
+        self.disk, self.file, self.id = disk, id != -1, (id if id != -1 else None)
+        self.indices = list(range(start, start+size))if indices is None else sorted(indices)
+        self.spans = spans(self.indices, len(self)) if self._with_spans else None            
     
     def __len__(self):
         return len(self.indices)
@@ -46,32 +36,22 @@ class File:
         if self._maintain_disk:
             self.disk[i] = str(self.id) if self.file else "."
         if self._with_spans:
-            check_merge = False
             for j, (start, end) in enumerate(self.spans.items()):
-                overshoot = i - (end - 1)
-                undershoot = start - i
-                if overshoot == 1 or undershoot == 1:
-                    check_merge = True
+                if ((overshoot := i - (end - 1)) == 1) | ((undershoot := start - i) == 1) | (overshoot > 1):
                     break
-                if overshoot > 1:
-                    break
-            if check_merge:
+            else:
+                self.spans[i] = i + 1
+                return self
+            if overshoot == 1 or undershoot == 1:
                 if overshoot == 1:
                     next_start, next_end = self.spans.peekitem(j + 1)
                     if (next_start - end) <= 2:
                         self.spans.pop(next_start)
-                        self.spans[start] = next_end                   
-                    else:
-                        self.spans[start] += 1
+                    self.spans[start] = next_end if (next_start - end) <= 2 else self.spans[start] + 1
                 if undershoot == 1:
                     prev_start, prev_end = self.spans.peekitem(j - 1)
                     self.spans.pop(start)
-                    if (start - prev_end) <= 2:
-                        self.spans[prev_start] = end
-                    else:
-                        self.spans[start - 1] = end
-            else:
-                self.spans[i] = i + 1
+                    self.spans[(prev_start if (start - prev_end) <= 2 else (start - 1))] = end                
         return self
     
     def __isub__(self, i : int):
@@ -86,13 +66,11 @@ class File:
                 self.spans.pop(start)
             else:
                 if i == start:
-                    self.spans.pop(start)
-                    self.spans[start + 1] = end
+                    self.spans[start + 1] = self.spans.pop(start)
                 elif i == end:
                     self.spans[start] -= 1
                 else:
-                    self.spans[start] = i
-                    self.spans[i+1] = end                    
+                    self.spans[start], self.spans[i+1] = i, end                    
         return self
     
     def swap(self, other : "File", i : int, j : int):
@@ -105,41 +83,33 @@ class File:
             raise RuntimeError(f"Swap index ({i}) must be present in left or right object")
         
     def checksum(self):
-        if not self.file:
-            raise TypeError("Cannot compute checksum for space-type file", self)
-        id = int(self.id)
-        return sum([i * id for i in self.indices])
+        assert self.file, TypeError("Cannot compute checksum for space-type file", self)
+        return sum([i * int(self.id) for i in self.indices])
         
     def __repr__(self):
-        name = "FILE" if self.file else "SPACE"
-        if self.file:
-            name += f"[{self.id}]"
+        name = f"FILE[{self.id}]" if self.file else "SPACE"
         return f"{name} | [{','.join(map(str, self.indices))}]"
-        
 
-def part1(data : list[int], visualize : slice | bool | None=False):    
-    ids = [int(i // 2) if i % 2 == 0 else -1 for i in range(len(data))]
-    disk = expand_view(data, ids)
-    n = len(disk)
+def preprocess(data : list[int], visualize : slice | bool | None=False):
+    disk, vis = expand_view(data, [int(i // 2) if i % 2 == 0 else -1 for i in range(len(data))]), None
     
     if isinstance(visualize, bool):
-        visualize = None if not visualize else slice(0, n, 1)
+        visualize = None if not visualize else slice(0, len(disk), 1)
     if not visualize is None:
-        vis = "|".join(disk[visualize])
-        print(vis)
+        print(vis := "|".join(disk[visualize]), end="")
     
     files, space = {}, []
-    for i, e in enumerate(disk):
-        if e == ".":
-            space.append(i)
-        else:
-            if not e in files:
-                files[e] = []
-            files[e].append(i)
+    [space.append(i) if e == "." else files.update({e : files.get(e, []) + [i]}) for i, e in enumerate(disk)]
+            
+    return disk, files, space, visualize, vis
+            
+
+def part1(data : list[int], visualize : slice | bool | None=False):
+    disk, files, space, visualize, vis = preprocess(data, visualize)
     files = {str(id) : File(id, disk, indices=i) for id, i in files.items()}
     space = File(-1, disk, indices=space)
-    i, j = 0, n - 1
-    while True:
+    j = len(disk) - 1
+    for i in range(j):
         if disk[i] == ".":
             while not disk[j].isdigit():
                 j -= 1
@@ -147,56 +117,34 @@ def part1(data : list[int], visualize : slice | bool | None=False):
                 break
             space.swap(files[disk[j]], i, j)
             if not visualize is None:
-                next_vis = "|".join(disk[visualize])
-                if vis != next_vis:
-                    vis = next_vis
-                    print(vis)
-        i += 1
+                if vis != (next_vis := "\r" + "|".join(disk[visualize])):
+                    time.sleep(0.2)
+                    print(vis := next_vis, end="")
+    if not visualize is None:
+        print()
     return sum([file.checksum() for _, file in files.items()])
     
 # print(part1(get_input(), True))
 print(part1(get_input("input")))
 
-from tqdm import tqdm
-
 def part2(data : list[int], visualize : slice | bool | None=False):    
-    ids = [int(i // 2) if i % 2 == 0 else -1 for i in range(len(data))]
-    disk = expand_view(data, ids)
-    n = len(disk)
-    
-    if isinstance(visualize, bool):
-        visualize = None if not visualize else slice(0, n, 1)
-    if not visualize is None:
-        vis = "|".join(disk[visualize])
-        print(vis)
-    
-    files, space = {}, []
-    for i, e in enumerate(disk):
-        if e == ".":
-            space.append(i)
-        else:
-            if not e in files:
-                files[e] = []
-            files[e].append(i)
+    disk, files, space, visualize, vis = preprocess(data, visualize)
     files = SortedDict({int(id) : File(id, disk, indices=i, maintain_disk=not visualize is None) for id, i in files.items()})
     space = File(-1, disk, indices=space, maintain_disk=not visualize is None, with_spans=True)
-    
-    iterator = tqdm(reversed(files), total=len(files)) if visualize is None else reversed(files)
-    for id in iterator:
+    for id in reversed(files):
         file = files[id]
         for start in space.spans:
             if start > file.indices[0]:
                     break
-            end = space.spans[start]
-            if (end - start) >= len(file):
-                for i, j in zip(range(start, start + len(file)), file.indices.copy()):
-                    space.swap(file, i, j)
+            if (space.spans[start] - start) >= len(file):
+                [space.swap(file, i, j) for i, j in zip(range(start, start + len(file)), file.indices.copy())]
                 break
         if not visualize is None:
-            next_vis = "|".join(disk[visualize])
-            if vis != next_vis:
-                vis = next_vis
-                print(vis)
+            if vis != (next_vis := "\r" + "|".join(disk[visualize])):
+                time.sleep(0.2)
+                print(vis := next_vis, end="")
+    if not visualize is None:
+        print()
     return sum([file.checksum() for _, file in files.items()])
 
 # print(part2(get_input(), True))
