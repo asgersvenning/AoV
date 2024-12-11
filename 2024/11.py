b9372eece5b4efbadf8b02ebfc6f0097a62ad6c9
asgersvenning <asgersvenning@gmail.com>
2024-12-11 20:53:49 +0100

    Day 11: Debloated!

--- 2024/11.py
+++ 2024/11.py
@@ -4,38 +4,31 @@ def parse_input(type : str):
     return list(map(int, get_lines(get_path(type))[0].split(" ")))
 
 def split(num : int):
-    num = str(num)
-    return int(num[:(half := len(num) // 2)]), int(num[half:])
+    return (int(num[:(half := len(num) // 2)]), int(num[half:])) if (num := str(num)) else (-1, -1)
 
 def transmute(num : int):
-    if num == 0:
-        return (1, )
-    if len(str(num)) % 2 == 0:
-        return split(num)
-    return (num * 2024, )
+    return (1, ) if num == 0 else split(num) if len(str(num)) % 2 == 0 else (num * 2024, )
 
 def blink(sequence : list[int]):
     return [new for stone in sequence for new in transmute(stone)]
 
 def part1(sequence : list[int], n : int=25):
-    if n == 1:
-        return len(blink(sequence))
-    return len([sequence := blink(sequence) for i in range(n)][-1])
+    return len([sequence := blink(sequence) for _ in range(n)][-1]) if n > 1 else len(blink(sequence))
 
 print(part1(parse_input("test")))
 # print(part1(parse_input("input")))
 
-cache = []
+cache = {}
+
+def update_cache(stone : int, n : int):
+    global cache
+    if n not in cache:
+        cache[n] = {}
+    if stone not in cache[n]:
+        cache[n][stone] = sum(blink1_many(new, n - 1) for new in transmute(stone))
 
 def blink1_many(stone : int, n : int):
-    global cache
-    if n == 0:
-        return 1
-    while n >= len(cache):
-        cache += [{}]    
-    if not stone in cache[n]:
-        cache[n][stone] = sum(blink1_many(new, n - 1) for new in transmute(stone))
-    return cache[n][stone]
+    return 1 if n == 0 or not update_cache(stone, n) is None else cache[n][stone]
 
 def part2(sequence : list[int], n : int=75):
     return sum(blink1_many(stone, n) for stone in sequence)
